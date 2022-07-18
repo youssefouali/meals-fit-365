ee98434b0d90107cc23f81215fc74fb7182d4177
youssefouali <youssef.ouali@esprit.tn>
2022-07-18 01:08:06 +0100

    First commit

---
+++ src/main/java/com/mealsfitbackend/MealsFitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mealsfitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealsFitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealsFitBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mealsfitbackend/MealsFitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mealsfitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealsFitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
